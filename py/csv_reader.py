bb711408151f2136cb1ea8ca3cc15b6b4c7dfa39
LandyCuadra <orlandocuadra350@hotmail.com>
2023-07-14 09:52:47 -0700

    fixed typo

--- py/csv_reader.py
+++ py/csv_reader.py
@@ -1,7 +1,7 @@
 import csv
 
 csv_result = []
-with open('C:/RocketbotProject/AmazonScrapper/files/productInput.csv', encoding='utf-8') as csv_file:
+with open('C:/RocketbotProject/AmazonScraper/files/productInput.csv', encoding='utf-8') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     for row in csv_reader:
         csv_result.append(row)
--- py/csv_writer.py
+++ py/csv_writer.py
@@ -5,7 +5,7 @@ result = {result}
 csv_name = f'{product}_prices.csv'
 fields = list(result[0].keys())
 
-with open(f'C:/RocketbotProject/AmazonScrapper/files/result/{csv_name}', mode='w', encoding='utf-8', newline='') as csv_file:
+with open(f'C:/RocketbotProject/AmazonScraper/files/result/{csv_name}', mode='w', encoding='utf-8', newline='') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fields)
     csv_writer.writeheader()
     csv_writer.writerows(result)
